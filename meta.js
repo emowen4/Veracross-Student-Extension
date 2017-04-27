abbeb949acba58bbb2c7097198ee8a45e601992c
Owen Wang <owenwangwyx@gmail.com>
2017-04-27 16:56:38 -0400

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Veracross Score Reminder
+// @namespace    https://github.com/emowen/
+// @version      1.2
+// @description  Check the update of score in Veracross periodically.
+// @homepage     https://github.com/emowen/Veracross-Score-Reminder
+// @author       EmOwen4
+// @updateURL    https://github.com/emowen/Veracross-Score-Reminder/blob/master/Veracross-Score-Reminder.js
+// @downloadURL  https://github.com/emowen/Veracross-Score-Reminder/blob/master/Veracross-Score-Reminder.js
+// @match        https://portals.veracross.com/*/student
+// @match        https://portals.veracross.com/*/student?id=*
+// @run-at       document-body
+// @grant        window.focus
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_listValues
+// ==/UserScript==

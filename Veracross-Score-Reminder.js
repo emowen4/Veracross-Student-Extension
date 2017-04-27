8eae18cffd04d3cddf7145bfaeca00526746b5b5
Owen Wang <owenwangwyx@gmail.com>
2017-04-27 16:50:22 -0400

    Update Veracross-Score-Reminder.js

--- Veracross-Score-Reminder.js
+++ Veracross-Score-Reminder.js
@@ -3,7 +3,9 @@
 // @namespace    http://tampermonkey.net/
 // @version      2
 // @description  Check the update of score in Veracross periodically.
+// @homepage     https://github.com/emowen/Veracross-Score-Reminder
 // @author       EmOwen4
+// @updateURL    https://github.com/emowen/Veracross-Score-Reminder/blob/master/Veracross-Score-Reminder.js
 // @match        https://portals.veracross.com/*/student
 // @match        https://portals.veracross.com/*/student?id=*
 // @run-at       document-body
